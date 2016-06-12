25ad4d33f5751e5629305cb567fd5a4dac597e98
Peter Velkov <kidroca@gmail.com>
2016-06-13 00:57:14 +0300

    Refactored the ConsoleHelper.cs Moved the Restart() method from ConsoleMio Added documentation

--- ConsoleHelper.cs
+++ ConsoleHelper.cs
@@ -1,11 +1,33 @@
 ﻿namespace HomeworkHelpers
 {
+    using System;
     using ConsoleEnchantments;
 
+    /// <summary>
+    /// A helper for Console Applications
+    /// </summary>
     public abstract class ConsoleHelper
     {
         private ConsoleMio console;
 
+        /// <summary>
+        /// Holds the ConsoleMio single instance
+        /// </summary>
         public ConsoleMio ConsoleMio => this.console ?? (this.console = new ConsoleMio());
+
+        /// <summary>
+        /// Restarts the given method, usually for Main.
+        /// </summary>
+        /// <param name="restartCallback"></param>
+        public void Restart(Action restartCallback)
+        {
+            Console.WriteLine();
+            this.console.PrintHeading("PRESS ANY KEY TO RESTART or Ctrl + C to exit");
+            Console.CursorVisible = false;
+            Console.ReadKey(true);
+            Console.CursorVisible = true;
+
+            restartCallback();
+        }
     }
 }
