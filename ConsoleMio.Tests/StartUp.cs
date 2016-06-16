1b569fb35dda80afc44f39dba407adc00f9065e2
Peter Velkov <kidroca@gmail.com>
2016-06-16 10:09:59 +0300

    Reorganised folder structure of the entire solution

---
+++ ConsoleMio.Tests/StartUp.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleMio.Tests
+{
+    using ConsoleEnhancements;
+    using ConsoleEnhancements.Contracts;
+
+    public class StartUp
+    {
+        private static readonly IConsoleHombre Hombre = new ConsoleHombre(new ConsoleWriter());
+
+        private static void Main()
+        {
+            TestHeading();
+        }
+
+        private static void TestHeading()
+        {
+            Hombre.PrintHeading("Even heading");
+
+            Hombre.PrintHeading("Odd heading");
+
+            Hombre.PrintHeading("Some really long heading text that will probably span multiple lines. Dobar si pesho. Some really long heading text that will probably span multiple lines.");
+        }
+    }
+}
